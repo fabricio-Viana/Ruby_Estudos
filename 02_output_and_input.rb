29c54db48a49aa2a842c48a08b8ad27475a49f09
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2025-03-07 16:50:41 -0300

    Update 02_output_and_input.rb

--- 02_output_and_input.rb
+++ 02_output_and_input.rb
@@ -8,3 +8,8 @@ puts "Bom dia qual é o seu nome ?"
 nome = gets.chomp!
 
 puts "É um prazer conhece-lo senhor #{nome}"
+
+# temos 3 formas de ter saida de dados 
+puts "Hello World"
+p "Hello World"
+print "Hello Worls"
