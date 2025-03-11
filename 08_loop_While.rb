35477ff756d1b962a660667473620fe91e96aff6
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2025-03-11 17:01:52 -0300

    Create 08_loop_While.rb

---
+++ 08_loop_While.rb
@@ -0,0 +1,17 @@
+puts "João - Ei Pedro conte até 100 o mais rapido que vc conseguir \n"
+
+puts "Pedro - certo"
+
+contagem = 0
+
+while contagem < 100 do
+    contagem += 1
+    if contagem > 89
+    print contagem.to_s
+    else
+    print contagem.to_s+","
+    end
+end
+puts " ... ufa terminei\n"
+
+puts "João - É Pedro, não valeu você se perdeu na conta"
