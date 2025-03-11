2c95874d0ddbdfa7669c13d36fc37abae3822caf
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2025-03-11 14:05:52 -0300

    Create 03_if_simples.rb

---
+++ 03_if_simples.rb
@@ -0,0 +1,11 @@
+puts "me fale de 1 a 10 como foi seu dia"
+nota = gets.to_i
+ if nota <= 5
+     puts "parece que seu dia não foi bom"
+     
+elsif nota >= 6 && nota <= 9
+    puts "seu dia foi Bom"
+
+elsif nota == 10
+    puts "Seu dia foi incrivel!"
+end    
