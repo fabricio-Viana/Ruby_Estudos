a08bc92fe45a31a8b3b66f8b745741e3aecf3141
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2025-03-12 15:25:05 -0300

    Create 13_Array01.rb

---
+++ 13_Array01.rb
@@ -0,0 +1,47 @@
+array_numeros = [1,2,3,4,5]
+
+array_string = ["Michel"," Jacson"," da Silva"," Junior"]
+
+array_objeto= Array.new(5,10)
+
+print array_numeros
+
+puts ""
+
+print array_string
+
+puts ""
+
+print array_objeto
+
+puts ""
+
+print array_string[0]
+print array_string[1]
+print array_string[-1]
+print array_string[-2]
+
+puts ""
+
+aray_ano = [2,0,2,5]
+
+print aray_ano.first(2)
+print aray_ano.last(2)
+
+puts ""
+
+array_numeros.push(6)
+array_numeros <<  7
+print array_numeros
+
+puts ""
+
+array_numeros.pop
+
+print array_numeros
+
+array_numeros.pop(6)
+
+puts ""
+
+print array_numeros
