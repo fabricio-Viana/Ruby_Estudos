2eeba12b7ffd97b3040d0a56aeebff709d2466af
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2025-03-12 15:36:32 -0300

    Create 14_array02.rb

---
+++ 14_array02.rb
@@ -0,0 +1,23 @@
+numeros = [23,34,23,53,65]
+
+numeros.unshift(18)
+
+print numeros
+
+puts ""
+
+numeros.shift()
+
+print numeros
+
+puts ""
+
+a1 = [1,2,3,4,5]
+
+a2 = [2,4,6,8,10]
+
+print a1+a2
+
+puts ""
+
+print [1,2,3,4,5] - [1,3,5]
